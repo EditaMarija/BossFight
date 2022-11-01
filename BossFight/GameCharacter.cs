1d3151d942d58a8921d9020390d3840c3d7025e5
Edita Marija <52567746+EditaMarija@users.noreply.github.com>
2022-11-01 14:19:41 +0100

    Initial commit

---
+++ BossFight/GameCharacter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+
+        public GameCharacter()
+        {
+                
+        }
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+
+        public void Fight(GameCharacter Enemy)
+        {
+            int originalStrength = Enemy.Strength;
+            initBossStrength(Enemy.Strength);
+            int originalStamina = 0;
+
+            if ((this.Health >= 0 && this.Stamina >= 0) && (Enemy.Health >= 0))
+            {
+                Enemy.Health -= this.Strength;
+                this.Stamina -= 10;
+                Enemy.Strength = originalStrength + 10;
+                Console.WriteLine("Original stamina: " + originalStamina);
+                Console.WriteLine(Enemy.Name + " has " + Enemy.Health + "health");
+            }
+            else if (!(this.Stamina >= 0))
+            {
+                while(this.Stamina <= originalStamina)
+                {
+                    Recharge();
+                }
+            }
+
+            Enemy.Strength = originalStrength;
+        }
+
+        public void Recharge()
+        {
+            this.Stamina = this.Stamina + 10;
+            Console.WriteLine(this.Name + " now has " + this.Stamina);
+            Thread.Sleep(2000);
+
+        }
+
+        public void initBossStrength(int ogStrength)
+        {
+            Random n = new Random();
+            int strength = n.Next(0, ogStrength + 1);
+            ogStrength = strength;
+        }
+    }
+}
---
+++ BossFight/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Boss Fight!");
+
+            GameCharacter Hero = new GameCharacter(" Eragon ", 1000, 40, 30);
+            GameCharacter Boss = new GameCharacter(" Rykard ", 200, 20, 40);
+
+            Console.WriteLine(Boss.Strength);
+            while (true)
+            {
+                Hero.Fight(Boss);
+            }
+            
+        }
+
+    }
+}
